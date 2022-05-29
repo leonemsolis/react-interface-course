e7b1bbe3b7f0a853878efc9dc289298caf98608e
Ruslan Yun <leonemsolis@gmail.com>
2022-05-29 18:40:50 +0600

    Move AppointmentInfo to separate component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { BiCalendar, BiTrash } from "react-icons/bi";
+import { BiCalendar } from "react-icons/bi";
 import AddAppointment from "./component/AddAppointment";
+import AppointmentInfo from "./component/AppointmentInfo";
 import Search from "./component/Search";
 import appointmentsData from "./data.json";
 
@@ -14,21 +15,7 @@ function App() {
 
       <ul className="divide-y divide-gray-200">
         {
-          appointmentsData.map(appointment => (
-            <li className="px-3 py-3 flex items-start">
-              <button type="button"
-                className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                <BiTrash /></button>
-              <div className="flex-grow">
-                <div className="flex items-center">
-                  <span className="flex-none font-medium text-2xl text-blue-500">{appointment.petName}</span>
-                  <span className="flex-grow text-right">{appointment.aptDate}</span>
-                </div>
-                <div><b className="font-bold text-blue-500">Owner:</b> {appointment.ownerName}</div>
-                <div className="leading-tight">{appointment.aptNotes}</div>
-              </div>
-            </li>
-          ))
+          appointmentsData.map(appointment => <AppointmentInfo key={appointment.id} appointment={appointment} />)
         }
       </ul>
     </div>
